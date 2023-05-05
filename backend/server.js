07e19268dbf785946c1eaf129e7270b23b1da1e6
Rob Merrill <robgmerrill@gmail.com>
2023-05-05 15:02:04 -0700

    Add basic express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Server...')
+// commonjs module syntax
+// reactjs will use import
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to the support desk api'});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on PORT ${PORT}`)
+})
