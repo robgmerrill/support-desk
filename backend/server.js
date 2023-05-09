bc77662b30243469b6f4311fa0b9aa9d48a651de
Rob Merrill <robgmerrill@gmail.com>
2023-05-09 08:14:31 -0700

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 // commonjs module syntax
 // reactjs will use import
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+// Connect to database
+connectDB();
 const app = express();
 
 app.use(express.json())
